a81e540f73398e61842373dd9a1a676fbe16e3d6
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2025-09-06 23:41:49 +0530

    brute force of same intersection arr

--- intersection_arr.py
+++ intersection_arr.py
@@ -34,3 +34,28 @@
 # arr2 = [2, 3, 5, 7]
 
 # print("Intersection:", intersection_optimal(arr1, arr2))
+
+# 🔹 Brute Force Idea:
+
+# Loop through every element of the first array.
+
+# For each element, check if it exists in the second array.
+
+# If yes, add it to the result (avoid duplicates if needed).
+
+# Time Complexity → O(n × m) (because of nested loops).
+# def intersection_bruteforce(arr1, arr2):
+#     result = []
+#     for i in arr1:
+#         for j in arr2:
+#             if i == j:
+#                 if i not in result:   # to avoid duplicates
+#                     result.append(i)
+#     return result
+
+
+# # Example usage
+# arr1 = [1, 2, 4, 5, 6]
+# arr2 = [2, 3, 5, 7]
+
+# print("Intersection:", intersection_bruteforce(arr1, arr2))
