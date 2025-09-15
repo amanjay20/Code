0bddb08e4ed49570fa8b221cc8480eee64f53af1
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2025-09-15 22:59:04 +0530

    next_permutation

---
+++ next_permutation.py
@@ -0,0 +1,24 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        n = len(nums)
+        index = -1
+        for i in range (n-2,-1,-1):
+            if nums[i] < nums[i+1]:
+                index = i
+                break
+        if index == -1:
+            nums.reverse()
+            return nums
+
+        for i in range (n-1,index,-1):
+            if nums[i] > nums[index]:
+                nums[i],nums[index] = nums[index],nums[i]
+                break
+        left = index+1 
+        right = n-1
+        while left <= right:
+            nums[left],nums[right] = nums[right],nums[left]
+            left += 1
+            right -= 1   
+        return nums
+        
