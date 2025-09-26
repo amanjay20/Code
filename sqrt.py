27ee30ef0bc1bf3e1a14b1cbad22a34197b1718f
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2025-09-26 23:49:55 +0530

    sqrt_optimial

--- sqrt.py
+++ sqrt.py
@@ -4,3 +4,23 @@ class Solution:
         while i * i <= x:
             i += 1
         return i - 1
+    
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        
+        left, right = 1, x // 2
+        ans = 0
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid <= x:
+                ans = mid     # mid is a possible answer
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return ans
