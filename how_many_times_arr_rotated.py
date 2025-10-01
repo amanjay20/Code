e0e29ff337227e72abebbfcc44b4cec87642e605
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2025-10-01 23:53:00 +0530

    how_many_times_array_rotaed

---
+++ how_many_times_arr_rotated.py
@@ -0,0 +1,26 @@
+#        #  low = 0
+#         # high = len(nums)-1
+#         # ans = float('inf')
+# index = -1
+#         # while low <= high:
+#         #     mid = (low+high)//2
+#         #     # //search space is already sorted
+#         #     if nums[low] <= nums[high]:
+#                if arr[low] < ans:
+#                              index = low
+#                              ans = arr[low]
+#         #         break
+#         #     if nums[low] <= nums[mid]:
+#         #          if arr[low] < ans:
+#                              index = low
+#                              ans = arr[low]
+#                     else:
+#         #                low = mid + 1
+#         #     else:
+#         #          if arr[mid] < ans:
+#                              index = mid
+#                              ans = arr[mid]
+#                    else:
+
+#         #         high = mid - 1
+#         # return index
